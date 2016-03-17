a79fb672ae8f9aa2d4c416999139bfe637709bd5
Kirpal Makanga <makanga.kirpal@gmail.com>
2016-03-17 18:02:29 +0100

    added polyfills

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,50 @@ const gulpLoadPlugins = require('gulp-load-plugins');
 const $ = gulpLoadPlugins();
 const runSequence = require('run-sequence');
 const browserSync = require('browser-sync').create();
+const strip = require('gulp-strip-comments');
 
 gulp.task('clean', require('del').bind(null, ['dist']));
 
+gulp.task('concat', () => {
+  return gulp.src(['bower_components/es6-promise/es6-promise.js', 'bower_components/fetch/fetch.js', './lib/*.js'])
+
+  .pipe(gulp.dest('./dist/'));
+});
+
 gulp.task('lint', () => {
   return gulp.src('js/*').pipe($.eslint({
-    'parserOptions': {
+      'parserOptions': {
         'ecmaVersion': 6
-    },
-    'rules':{
+      },
+      'rules': {
         'quotes': [1, 'single'],
         'semi': [1, 'always']
-    }
-  }))
-  .pipe($.eslint.format())
-  // Brick on failure to be super strict
-  .pipe($.eslint.failOnError());
+      }
+    }))
+    .pipe($.eslint.format())
+    // Brick on failure to be super strict
+    .pipe($.eslint.failOnError());
 });
 
 gulp.task('babel', () => {
-	return gulp.src('js/*.js')
-		.pipe($.babel({
-			presets: ['es2015']
-		}))
-		.pipe(gulp.dest('dist'));
+  return gulp.src([
+      'bower_components/es6-promise/es6-promise.js',
+      'bower_components/fetch/fetch.js',
+      'js/ajaxloader.js'
+    ])
+    .pipe($.concat('ajaxloader.js'))
+    .pipe(strip())
+    .pipe($.babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build', ['lint', 'babel'], () => {
-  return gulp.src('dist').pipe($.size({title: 'build', gzip: false}));
+  return gulp.src('dist').pipe($.size({
+    title: 'build',
+    gzip: false
+  }));
 });
 
 gulp.task('watch', ['lint', 'babel'], () => {
